98439834eb7be6d4ef45b293f4625e91b8db2943
Snehan-Gn <pr46eml@eduvaud.ch>
2026-08-26 12:13:08 +0200

    feat(Esport): Création des classes

---
+++ snehan/EsportApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataSeries;
+using EsportApp;
+
+var valorantMatches = new[]
+{
+    new ValorantMatch("Léa", "Jett",  18, 6, 4, 8,  13, true),
+    new ValorantMatch("Léa", "Reyna", 22, 8, 2, 11,  9, false),
+    new ValorantMatch("Léa", "Neon",  20, 7, 5,  9, 13, true),
+};
+
+var valorant = DataSeries<ValorantMatch>.From(valorantMatches);
+Console.WriteLine(valorant.Count);
---
+++ snehan/EsportApp/ValorantMatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsportApp
+{
+    public class ValorantMatch
+    {
+        public ValorantMatch(string player, string agent, int kills, int deaths, int assits, int headshots, int rounds_won, bool won)
+        {
+            Player = player;
+            Agent = agent;
+            Kills = kills;
+            Deaths = deaths;
+            Assits = assits;
+            Headshots = headshots;
+            Rounds_won = rounds_won;
+            Won = won;
+        }
+
+        public string Player { get; }
+        public string Agent { get; }
+        public int Kills { get; }
+        public int Deaths { get; }
+        public int Assits { get; }
+        public int Headshots { get; }
+        public int Rounds_won { get; }
+        public bool Won { get; }
+    }
+}
