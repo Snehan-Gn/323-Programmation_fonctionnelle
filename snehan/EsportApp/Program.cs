0936edf96b7f22133b71d37a0cc2e57a8b6748bc
Snehan-Gn <pr46eml@eduvaud.ch>
2026-09-16 11:31:00 +0200

    chore(Program): Conversion du programme afin de récupérer les arguments en CLI

--- snehan/EsportApp/Program.cs
+++ snehan/EsportApp/Program.cs
@@ -1,85 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using DataSeries;
 using EsportApp;
 using System.Linq;
 
-ValorantMatch ParseValorant(string[] cols) => new ValorantMatch(
-    DateTime.Parse(cols[0]),
-    cols[1],
-    cols[2],
-    int.Parse(cols[3]),
-    int.Parse(cols[4]),
-    int.Parse(cols[5]),
-    int.Parse(cols[6]),
-    int.Parse(cols[7]),
-    bool.Parse(cols[8])
-);
+namespace EsportApp
+{
+    class Program
+    {
+        static ValorantMatch ParseValorant(string[] cols) => new ValorantMatch(
+            DateTime.Parse(cols[0]),
+            cols[1],
+            cols[2],
+            int.Parse(cols[3]),
+            int.Parse(cols[4]),
+            int.Parse(cols[5]),
+            int.Parse(cols[6]),
+            int.Parse(cols[7]),
+            bool.Parse(cols[8])
+        );
 
-Cs2Match ParseCs2(string[] cols) => new Cs2Match(
-    DateTime.Parse(cols[0]),
-    cols[1],
-    cols[2],
-    cols[3],
-    int.Parse(cols[4]),
-    int.Parse(cols[5]),
-    int.Parse(cols[6]),
-    int.Parse(cols[7]),
-    bool.Parse(cols[8])
-);
+        static Cs2Match ParseCs2(string[] cols) => new Cs2Match(
+            DateTime.Parse(cols[0]),
+            cols[1],
+            cols[2],
+            cols[3],
+            int.Parse(cols[4]),
+            int.Parse(cols[5]),
+            int.Parse(cols[6]),
+            int.Parse(cols[7]),
+            bool.Parse(cols[8])
+        );
 
-LolMatch ParseLol(string[] cols) => new LolMatch(
-    DateTime.Parse(cols[0]),
-    cols[1],
-    cols[2],
-    cols[3],
-    int.Parse(cols[4]),
-    int.Parse(cols[5]),
-    int.Parse(cols[6]),
-    int.Parse(cols[7]),
-    int.Parse(cols[8]),
-    bool.Parse(cols[9])
-);
+        static LolMatch ParseLol(string[] cols) => new LolMatch(
+            DateTime.Parse(cols[0]),
+            cols[1],
+            cols[2],
+            cols[3],
+            int.Parse(cols[4]),
+            int.Parse(cols[5]),
+            int.Parse(cols[6]),
+            int.Parse(cols[7]),
+            int.Parse(cols[8]),
+            bool.Parse(cols[9])
+        );
 
-var valorant = DataSeries<ValorantMatch>.FromCsv("data/valorant.csv", ParseValorant);
-var cs2 = DataSeries<Cs2Match>.FromCsv("data/cs2.csv", ParseCs2);
-var lol = DataSeries<LolMatch>.FromCsv("data/lol.csv", ParseLol);
+        static void Main(string[] args)
+        {
+            var valorant = DataSeries<ValorantMatch>.FromCsv("data/valorant.csv", ParseValorant);
+            var cs2 = DataSeries<Cs2Match>.FromCsv("data/cs2.csv", ParseCs2);
+            var lol = DataSeries<LolMatch>.FromCsv("data/lol.csv", ParseLol);
 
-var valorantValid = valorant.RemoveOutliers(m =>
-    m.Kills >= 0 && m.Kills <= 50 &&
-    m.Deaths >= 1 && m.Deaths <= 30 &&
-    m.Assists >= 0
-);
+            var valorantValid = valorant.RemoveOutliers(m =>
+                m.Kills >= 0 && m.Kills <= 50 &&
+                m.Deaths >= 1 && m.Deaths <= 30 &&
+                m.Assists >= 0
+            );
 
-var cs2Valid = cs2.RemoveOutliers(m =>
-    m.Kills + m.Assists <= 50 &&
-    m.Deaths >= 1
-);
+            var cs2Valid = cs2.RemoveOutliers(m =>
+                m.Kills + m.Assists <= 50 &&
+                m.Deaths >= 1
+            );
 
-var lolValid = lol.RemoveOutliers(m =>
-    m.Kills <= 10 &&
-    m.Deaths >= 1 &&
-    m.Assists >= 0 &&
-    m.Cs >= 0
-);
+            var lolValid = lol.RemoveOutliers(m =>
+                m.Kills <= 10 &&
+                m.Deaths >= 1 &&
+                m.Assists >= 0 &&
+                m.Cs >= 0
+            );
 
-Console.WriteLine($"Valorant : {valorantValid.Count} matchs");
-Console.WriteLine($"CS2  {cs2Valid.Count} matchs");
-Console.WriteLine($"LoL : {lolValid.Count} matchs");
-Console.WriteLine(valorantValid.HasAny(m => m.Kills > 20));
-Console.WriteLine(lolValid.AllMatch(m => m.Deaths >= 1));
+            Console.WriteLine($"Valorant : {valorantValid.Count} matchs");
+            Console.WriteLine($"CS2  {cs2Valid.Count} matchs");
+            Console.WriteLine($"LoL : {lolValid.Count} matchs");
+            Console.WriteLine(valorantValid.HasAny(m => m.Kills > 20));
+            Console.WriteLine(lolValid.AllMatch(m => m.Deaths >= 1));
 
-/*
-var raphaelGenerated = MatchGenerator.GenerateCs2("Raphaël", 20);
-var michelGenerated = MatchGenerator.GenerateValorant("Michel", 20);
-var gabrielGenerated = MatchGenerator.GenerateLol("Gabriel", 20);
+            /*
+            var raphaelGenerated = MatchGenerator.GenerateCs2("Raphaël", 20);
+            var michelGenerated = MatchGenerator.GenerateValorant("Michel", 20);
+            var gabrielGenerated = MatchGenerator.GenerateLol("Gabriel", 20);
 
-Func<Cs2Match, bool> isValid = m =>
-    m.Kills + m.Assists <= 50;
+            Func<Cs2Match, bool> isValid = m =>
+                m.Kills + m.Assists <= 50;
 
-var raphaelValid = raphaelGenerated.Filter(isValid);
-Console.WriteLine($"Avant : {raphaelGenerated.Count}, après : {raphaelValid.Count}");
+            var raphaelValid = raphaelGenerated.Filter(isValid);
+            Console.WriteLine($"Avant : {raphaelGenerated.Count}, après : {raphaelValid.Count}");
 
-ExportCSV.ExportCs2(raphaelValid, "raphael_generated.csv");
-ExportCSV.ExportValorant(michelGenerated, "michel_generated.csv");
-ExportCSV.ExportLol(gabrielGenerated, "gabriel_generated.csv");
-*/
+            ExportCSV.ExportCs2(raphaelValid, "raphael_generated.csv");
+            ExportCSV.ExportValorant(michelGenerated, "michel_generated.csv");
+            ExportCSV.ExportLol(gabrielGenerated, "gabriel_generated.csv");
+            */
+        }
+    }
+}
